ab59287e4eee122abf0fcf5fc9ddb9ce3f32610c
GEOVANNA SILVA COSTA <geovanna.costa@e239.local>
2026-08-19 14:39:05 -0300

    gdfg

---
+++ aula-08-manipulacao-do-dom/01-selecionando-elementos-de-dom/script.js
@@ -0,0 +1,30 @@
+// JavaScript
+
+//Selecionando um elemento pela Tag
+const p0 = window.document.getElementsByTagName('p')[0];
+
+p0.style.color = "purple";
+p0.innerText = "Mudei o texto";
+
+const p1 = document.getElementsByTagName("p")[1];
+
+p1.style.color = "green";
+
+// Capturando o corpo do site
+const corpoSite = window.document.body;
+
+// Mudando sua cor
+corpoSite.style.background = "purple";
+
+// Acessando conteúdo de um elemento DOM
+document.write(`<br> No 2º parágrafo do site está escrito assim: ${p1.innerText}`);
+
+const div = getElementsById("pa2")[3];
+pa2.style.color = "purple";
+pa2.innerText = "Selecionado elementos pelo ID";
+
+
+const = getElementsByClassName()
+
+
+
