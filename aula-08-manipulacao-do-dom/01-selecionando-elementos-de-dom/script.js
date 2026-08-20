07d889b92be35f62b67c9afe458df8fb9b75a2fe
GEOVANNA SILVA COSTA <geovanna.costa@e239.local>
2026-08-20 10:54:31 -0300

    Finalizado

--- aula-08-manipulacao-do-dom/01-selecionando-elementos-de-dom/script.js
+++ aula-08-manipulacao-do-dom/01-selecionando-elementos-de-dom/script.js
@@ -1,29 +1,50 @@
 // JavaScript
 
 //Selecionando um elemento pela Tag
-const p0 = window.document.getElementsByTagName('p')[0];
+// const p0 = window.document.getElementsByTagName('p')[0];
 
-p0.style.color = "purple";
-p0.innerText = "Mudei o texto";
+// p0.style.color = "purple";
+// p0.innerText = "Mudei o texto";
 
-const p1 = document.getElementsByTagName("p")[1];
+// const p1 = document.getElementsByTagName("p")[1];
 
-p1.style.color = "green";
+// p1.style.color = "green";
 
-// Capturando o corpo do site
-const corpoSite = window.document.body;
+// // Capturando o corpo do site
+// const corpoSite = window.document.body;
 
-// Mudando sua cor
-corpoSite.style.background = "purple";
+// // Mudando sua cor
+// corpoSite.style.background = "black";
 
-// Acessando conteúdo de um elemento DOM
-document.write(`<br> No 2º parágrafo do site está escrito assim: ${p1.innerText}`);
+// // Acessando conteúdo de um elemento DOM
+// document.write(`<br> No 2º parágrafo do site está escrito assim: ${p1.innerText}`);
 
-const div = getElementsById("pa2")[3];
-pa2.style.color = "purple";
-pa2.innerText = "Selecionado elementos pelo ID";
+//ID
+const pa2 = document.getElementById("pa2");
+
+pa2.style.background = "purple";
+pa2.innerText = "Selecionado elemento pelo ID";
+
+//NOME
+const nome = document.getElementsByName("nome")[0];
+
+nome.style.background = "blue";
+nome.innerText = "Selecionado pelo nome";
+
+//CLASSE
+const classe = document.getElementsByClassName("classe")[0];
+
+classe.style.background = "red";
+classe.innerText = "Selecionado pela classe";
+
+//SELETOR
+const seletor = document.querySelector("p:nth-of-type(4)");
+
+seletor.style.color = "black";
+seletor.style.background = "yellow";
+seletor.innerText = "Selecionado pelo seletor";
 
 
-c
+
 
 
