bd47ae3a86cda604ad0dc90adcf11668df2a4de0
GEOVANNA SILVA COSTA <geovanna.costa@e239.local>
2026-08-26 13:54:23 -0300

    Fazendo

---
+++ aula-08-exercicio-02/script.js
@@ -0,0 +1,62 @@
+/* 
+  EXERCÍCIO: Seleção e Manipulação do DOM
+  ---------------------------------------
+  Objetivo: praticar como selecionar elementos do DOM e alterar sua aparência
+
+  Complete cada exercício abaixo escrevendo o código onde indicado.
+*/
+
+// ------------------------------------------------------
+// Exercício 1
+// Selecione o elemento com id "titulo" e:
+// - altere o texto para "Titulo Alterado"
+// - mude a cor do texto para azul
+// ------------------------------------------------------
+
+// seu código aqui
+
+ const titulo = document.getElementaryById("titulo");
+
+ titulo.innerText = "Título Alterado";
+ titulo.style.color = "blue";
+
+// ------------------------------------------------------
+// Exercício 2
+// Selecione TODOS os elementos com a classe "paragrafo"
+//e mude a cor de fundo (background-color) de todos eles para amarel
+// Dica: use querySelectorAll e um laço (for ou forEach) para
+// percorrer os elementos, já que são vários.
+// ------------------------------------------------------
+
+// seu código aqui
+
+// ------------------------------------------------------
+//Exercício 3
+// Selecione o elemento com id "caixa" e:
+// altere o innerText para "Conteúdo atualizado!"
+// mude a borda (border) para "3px solid red"
+//mude o tamanho da fonte (fontSize) para "20px"
+
+// -----------------------------------------------------
+
+// seu código aqui
+
+// -----------------------------------------------------
+// Exercicio 4
+// Selecione os itens da lista (id "lista")
+// usando querySelector e mude seu texto para "Item alterado"
+
+// -----------------------------------------------------
+
+// seu código aqui
+
+// -----------------------------------------------------
+// Exercício 5
+// Selecione o botão com id "botao" e:
+// - altere o texto para "Clique aqui"
+// - mude a cor de fundo para verde
+// - mude a cor do texto para branco
+
+// ------------------------------------------------------
+
+// seu código aqui
